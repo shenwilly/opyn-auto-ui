c15cc981b9b2a57da1c995f56fa324494640db08
willy <shen.willy95@gmail.com>
2021-07-29 15:37:49 +0700

    add endpoint and ropsten

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -2,12 +2,24 @@ import { ETHEREUM_CHAIN } from "../types";
 
 export enum CHAIN_ID {
   MAINNET = 1,
+  ROPSTEN = 3,
 }
 
-export const CHAIN_METADATA: {[key: number]: ETHEREUM_CHAIN} = {
-  1: {
+export const CHAIN_METADATA: {[key in CHAIN_ID]: ETHEREUM_CHAIN} = {
+  [CHAIN_ID.MAINNET]: {
     chainId: "0x01",
     chainName: "Ethereum Mainnet",
+    nativeCurrency: {
+      name: "Ether",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    rpcUrls: [],
+    blockExplorerUrls: [],
+  },
+  [CHAIN_ID.ROPSTEN]: {
+    chainId: "0x03",
+    chainName: "Ropsten",
     nativeCurrency: {
       name: "Ether",
       symbol: "ETH",
