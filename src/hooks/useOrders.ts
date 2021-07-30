3ab4ef03e680f31727cc97684485712b838bb858
willy <shen.willy95@gmail.com>
2021-07-30 11:30:29 +0700

    fix useOrders

--- src/hooks/useOrders.ts
+++ src/hooks/useOrders.ts
@@ -1,18 +1,66 @@
+import { BigNumber, ethers } from 'ethers'
 import { useState, useCallback, useEffect } from 'react'
-
 import { SubgraphOrder } from '../types'
-import { getOrders, getVaults } from '../utils/graph'
+import { getOrders } from '../utils/graph'
 import { CHAIN_ID } from '../constants/networks'
 import { DEFAULT_INTERVAL } from '../constants'
+import GammaRedeemerAbi from "../constants/abis/GammaRedeemer.json";
+import { GammaRedeemer } from '../types/eth';
+import { GAMMA_REDEEMER_ADDRESS } from '../constants/address'
+import useEthereum from './useEthereum'
+
+interface useOrdersResult {
+  createOrder: Function; 
+  cancelOrder: Function;
+  orders: SubgraphOrder[] | null; 
+  refetch: Function; 
+  isLoading: boolean;
+  fetchIsLoading: boolean;
+}
 
 export function useOrders(
   account: string,
   chainId: CHAIN_ID,
-): { orders: SubgraphOrder[] | null; refetch: Function; isLoading: boolean } {
+): useOrdersResult {
+  const { ethAccount, injectedProvider } = useEthereum();
   const [orders, setOrders] = useState<SubgraphOrder[]>([])
 
   const [refreshCount, setRefreshCount] = useState(0)
-  const [isLoading, setIsLoading] = useState(true)
+  const [fetchIsLoading, setFetchIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const getGammaRedeemer = (): GammaRedeemer => {
+    return (new ethers.Contract(GAMMA_REDEEMER_ADDRESS[chainId], GammaRedeemerAbi, ethAccount)) as GammaRedeemer;
+  }
+
+  const createOrder = useCallback(
+    async (otoken: string, amount: BigNumber, vaultId: BigNumber) => {
+    if (!ethAccount || !injectedProvider || otoken === "") {
+      return;
+    }
+  
+    setIsLoading(true);
+    const gammaRedeemer = getGammaRedeemer();
+    await gammaRedeemer.createOrder(otoken, amount, vaultId);
+    setIsLoading(false);
+    
+    },
+    [ethAccount, injectedProvider, getGammaRedeemer],
+  )
+
+  const cancelOrder = useCallback(
+    async (orderId: BigNumber) => {
+      if (!ethAccount || !injectedProvider) {
+        return;
+      }
+    
+      setIsLoading(true);
+      const gammaRedeemer = getGammaRedeemer();
+      await gammaRedeemer.cancelOrder(orderId);
+      setIsLoading(false);  
+    },
+    [ethAccount, injectedProvider, getGammaRedeemer],
+  )
 
   const refetch = useCallback(() => {
     setRefreshCount(count => count + 1)
@@ -22,7 +70,7 @@ export function useOrders(
     async function updateOrders() {
       const vaults = await getOrders(chainId, account);
       if (vaults === null) return
-      setIsLoading(false)
+      setFetchIsLoading(false)
       setOrders(orders)
     }
     updateOrders()
@@ -30,5 +78,5 @@ export function useOrders(
     return () => clearInterval(interval)
   }, [chainId, account, refreshCount])
 
-  return { orders, isLoading, refetch }
+  return { createOrder, cancelOrder, orders, isLoading, refetch, fetchIsLoading }
 }
