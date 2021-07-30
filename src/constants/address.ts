cb86f54873bce55e0371bfc1771e7d1decbfad3c
willy <shen.willy95@gmail.com>
2021-07-30 11:30:12 +0700

    add Gamma controller address

--- src/constants/address.ts
+++ src/constants/address.ts
@@ -4,3 +4,8 @@ export const GAMMA_REDEEMER_ADDRESS: {[key in CHAIN_ID]: string} = {
   [CHAIN_ID.MAINNET]: "",
   [CHAIN_ID.ROPSTEN]: "",
 };
+
+export const GAMMA_CONTROLLER_ADDRESS: {[key in CHAIN_ID]: string} = {
+  [CHAIN_ID.MAINNET]: "",
+  [CHAIN_ID.ROPSTEN]: "",
+};
