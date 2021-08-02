d95d79bb9add71185ad1af85d5873d3f1438db1e
willy <shen.willy95@gmail.com>
2021-08-02 20:26:35 +0700

    add tempaddress

--- src/constants/address.ts
+++ src/constants/address.ts
@@ -1,11 +1,11 @@
 import { CHAIN_ID } from ".";
 
 export const GAMMA_REDEEMER_ADDRESS: {[key in CHAIN_ID]: string} = {
-  [CHAIN_ID.MAINNET]: "",
-  [CHAIN_ID.ROPSTEN]: "",
+  [CHAIN_ID.MAINNET]: "0x9c08febE5979CBc0F5a04ec55c644fa9d9B3F27F",
+  [CHAIN_ID.ROPSTEN]: "0x9c08febE5979CBc0F5a04ec55c644fa9d9B3F27F",
 };
 
 export const GAMMA_CONTROLLER_ADDRESS: {[key in CHAIN_ID]: string} = {
-  [CHAIN_ID.MAINNET]: "",
-  [CHAIN_ID.ROPSTEN]: "",
+  [CHAIN_ID.MAINNET]: "0x7e9beaccdccee88558aaa2dc121e52ec6226864e",
+  [CHAIN_ID.ROPSTEN]: "0x7e9beaccdccee88558aaa2dc121e52ec6226864e",
 };
