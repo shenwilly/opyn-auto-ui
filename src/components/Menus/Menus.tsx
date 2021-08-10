a99a1b85ea117bc41ab34f3e34d8b1b56073a51e
willy <shen.willy95@gmail.com>
2021-08-10 23:30:15 +0700

    add page marker

--- src/components/Menus/Menus.tsx
+++ src/components/Menus/Menus.tsx
@@ -2,14 +2,18 @@ import { Button, HStack } from "@chakra-ui/react"
 import { useHistory } from "react-router-dom";
 
 const Menus = () => {
-  const { push } = useHistory();
+  const { push, location } = useHistory();
 
-    return (
-        <HStack justify="center">
-            <Button onClick={() => push("/redeem")}>Redeem oToken</Button>
-            <Button onClick={() => push("/settle")}>Settle Vault</Button>
-        </HStack>
-    );
+  return (
+      <HStack justify="center" spacing={5}>
+          <Button onClick={() => push("/redeem")} 
+            border={location.pathname === '/redeem' ? '1px' : ''}
+            >Redeem oToken</Button>
+          <Button onClick={() => push("/settle")} 
+            border={location.pathname === '/settle' ? '1px' : ''}
+            >Settle Vault</Button>
+      </HStack>
+  );
 };
 
 export default Menus;
